1869e03cf99ac6745cfcd29e4950311014e53c2e
Sam Ruby <rubys@intertingly.net>
2017-06-21 10:40:06 +0800

    After Task J4: Locale Switcher (All OK)

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,7 +4,11 @@ class StoreController < ApplicationController
   before_action :set_cart
   
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
     @count = increment_count
   end
   
