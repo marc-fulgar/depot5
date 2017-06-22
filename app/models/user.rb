e1020a48dba82000696e279126b7d315f761ccc8
marc-fulgar <marc.fulgar@adish.co.jp>
2017-06-22 16:48:17 +0800

    Devise Working (Needs improvement)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true, uniqueness: true
-  has_secure_password
+  #validates :name, presence: true, uniqueness: true
   
   after_destroy :ensure_an_admin_remains
   
