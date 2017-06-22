557a5e834ce128166c1ecc1bcd2466bfb31d0438
marc-fulgar <marc.fulgar@adish.co.jp>
2017-06-22 10:58:11 +0800

    Implemented will_paginate on USERS (All OK)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.order(:name)
+    @users = User.order('created_at desc').paginate(:page => params[:page], :per_page => 10)
   end
 
   # GET /users/1
